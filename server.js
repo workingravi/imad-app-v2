b66faf4b739c14c84cfd49ef7b1a95065af7f6e2
workingravi <work.ravis@gmail.com>
2017-02-02 15:48:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/article-1', function (req, res) {
   res.sendFile("artic 1");
 });
 
-app.get('/article-2', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile("artic 2");
 });
 app.get('/article-3', function (req, res) {
