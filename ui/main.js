cb98d985d730d93d8f6b91f6760bffdd57f02458
workingravi <work.ravis@gmail.com>
2017-03-02 15:18:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var counter = document.getElementById("clicker");
+
+var upCount = function(){
+    counter.textContent = counter.textContent + 1;
+};
+
+counter.addEventListener("click", upCount);
+
